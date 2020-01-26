7dac44787fff2c565d919a78d3d1256cd45d92f7
Bartłomiej Wolski <rudy10003@gmail.com>
2020-01-26 13:32:13 +0100

    commit 4

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import Multilayer_Perceptron
+import Perceptron_Training
 import read_file
 
 
 
 data_set = read_file.Data_set().read_file("forestfires.csv")
 
-print(data_set[0])
+
+print(data_set)
 
---
+++ Perceptron_Training.py
@@ -0,0 +1,8 @@
+import Multilayer_Perceptron
+
+
+
+class Perceptron_Training:
+
+    def __init__(self,training_data_set):
+        self.data_set = training_data_set
